7a543bb060035d3528408713f14570f1f3c5de7d
lshpaner <lshpaner@mednet.ucla.edu>
2025-03-18 16:28:17 -0700

    Installed ccds and migrated Makefile and .gitignore

---
+++ custom_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='cookie-cutter data science re-adapted to be used with the `model_tuner` library.',
+    author='Leonid Shpaner, UCLA CTSI',
+    license='MIT',
+)
